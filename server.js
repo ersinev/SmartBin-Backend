43a6634e36186246d90f3fd341bf97599e5cc194
ersinev <ersinevci@hotmail.com>
2023-08-29 09:44:31 +0200

    cors updated

--- server.js
+++ server.js
@@ -4,8 +4,9 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-
+const cors = require('cors');
 const app = express();
+app.use(cors())
 
 const PORT = process.env.PORT || 3000;
 
