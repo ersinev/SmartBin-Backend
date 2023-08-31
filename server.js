284e65fe2373d3736a16b8aa822069d0b773b914
ersinev <ersinevci@hotmail.com>
2023-08-31 11:03:55 +0200

    updated

--- server.js
+++ server.js
@@ -112,18 +112,18 @@ app.post('/send-email', async (req, res) => {
     let transporter = nodemailer.createTransport({
         service: 'hotmail',
         auth: {
-            user: process.env.EMAIL_USER,
-            pass: process.env.EMAIL_PASS,
+            user: 'itgaragesmartbin@hotmail.com',
+            pass: 'smart123456*',
         },
     });
     
     let mailOptions = {
-        from: process.env.EMAIL_USER,
+        from: 'itgaragesmartbin@hotmail.com',
         to: to,
         subject: subject,
         text: text,
     };
-    console.log(to)
+    
 
     
     transporter.sendMail(mailOptions, function (error, info) {
