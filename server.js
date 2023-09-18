da817988c76302822d32d91cbf2915d22ebb4930
ersinev <ersinevci@hotmail.com>
2023-09-18 11:28:57 +0200

    updated

--- server.js
+++ server.js
@@ -141,7 +141,7 @@ app.post("/send-email", async (req, res) => {
 
   // Create a nodemailer transporter
   const transporter = nodemailer.createTransport({
-    host: "smtp.office365.com",
+    host: "outlook",
     port: 587,
     secure: false, // STARTTLS
     auth: {
